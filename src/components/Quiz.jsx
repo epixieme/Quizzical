869f38372d4e743e79cfe7644844d4934570c904
epixieme <kirstie_hayes@hotmail.com>
2023-05-02 11:23:16 +0100

    added styling logic for correct answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,33 +1,37 @@
-export default function Quiz(props){
-
-    
-
-
-const answers = props.allAnswers.map(answer=>{
-//     // console.log(answer.id) how do I get this to work on click rather than straigt off
-    const styles ={
-        border: answer.isSelected? "1px solid green":"none",
-        backgroundColor:answer.isSelected && answer.isCorrect && props.isChecked? "green":"none"
-    }
-        return (
-                <section  className="answer-container" key={answer.id} style={styles}>
-                <p key={answer.id} onClick={()=>props.handleClick(answer.answer, answer.id, props.mainid)}>{answer.answer}</p>
-                </section>
-                )
-    
-})
-
-
-return(
-       <section key={props.id}>
-       <p key={props.id}>{props.question}</p>
-       <section className="answers-outer-container" key={props.id}>
-       {answers}
-       </section>
-     
-   
+export default function Quiz(props) {
+  const answers = props.allAnswers.map((answer) => {
+    //     // console.log(answer.id) how do I get this to work on click rather than straigt off
+    const styles = {
+      border: answer.isSelected ? "1px solid green" : "none",
+      backgroundColor:
+        answer.answer === answer.correctAnswer && props.isChecked
+          ? "green"
+          : answer.isSelected && answer.isCorrect && props.isChecked
+          ? "green"
+          : answer.isSelected && !answer.isCorrect && props.isChecked
+          ? "red"
+          : "none",
+    };
+    return (
+      <section className="answer-container" key={answer.id} style={styles}>
+        <p
+          key={answer.id}
+          onClick={() =>
+            props.handleClick(answer.answer, answer.id, props.mainid)
+          }
+        >
+          {answer.answer}
+        </p>
       </section>
-      
-        
-    )
+    );
+  });
+
+  return (
+    <section key={props.id}>
+      <p key={props.id}>{props.question}</p>
+      <section className="answers-outer-container" key={props.id}>
+        {answers}
+      </section>
+    </section>
+  );
 }
