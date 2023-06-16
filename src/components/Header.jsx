cad399091fa93c9ebf0f9c7143e656dd36aeb26c
epixieme <kirstie_hayes@hotmail.com>
2023-06-16 12:16:09 +0100

    added challenge folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(props) {
   return (
     <div className="main-screen">
       <h1>{props.title}</h1>
-      <p>Test Your Knowledge with the Ultimate Online Trivia Challenge!</p>
+      <p>Test Your Knowledge With The Ultimate Online Trivia Challenge!</p>
     </div>
   );
 }
